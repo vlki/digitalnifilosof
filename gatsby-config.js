54a80eb9eda1a6749ab8b82ece2c95a8812b242a
Jan Vlcek <vlki@vlki.cz>
2021-03-03 14:14:06 +0100

    Deps update, better site description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,14 @@
 module.exports = {
   siteMetadata: {
     title: `Digitální filosof`,
-    description: `Z anglických textů vybraných filosofů jsme složili korpusy, které jsme použili pro natrénování neuronové sítě specializované na pochopení a generování textu. Zkusili jsme tak stvořit virtuální osobnosti, „digitální filosofy,“ jejichž generované texty jsme analyzovali. Na tomto webu natrénované sítě zpřístupňujeme, abyste si mohli generování vyzkoušet sami.`,
+    description: `Natrénovali jsme neuronovou síť texty filosofů a ta je teď schopná generovat texty další. Projekt Studií nových médií na FF UK z podzimu 2019.`,
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [
-          {
-            family: `Merriweather`,
-            subsets: [`latin-ext`],
-            variants: [`400`, `400i`, `700`, `700i`, `900`, `900i`],
-          },
-        ],
+        fonts: [`Merriweather\:400,400i,700,700i,900,900i`],
+        display: "swap",
       },
     },
     `gatsby-plugin-styled-components`,
