c81361198c975f06870debdbfdf8a1d2d58d0ac4
Jan Vlcek <vlki@vlki.cz>
2023-07-30 12:59:36 +0200

    Remove plausible tracking

--- src/html.js
+++ src/html.js
@@ -12,12 +12,6 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
-        <script
-          async
-          defer
-          data-domain="digitalnifilosof.cz"
-          src="https://plausible.io/js/plausible.js"
-        ></script>
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
